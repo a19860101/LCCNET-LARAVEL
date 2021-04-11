2544dcee2c9272f49c0be4ed60cfa67ddbd1a2f7
Zac <a19860101@gmail.com>
2021-04-11 16:12:13 +0800

    20210411

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,18 @@ Route::post('/post/store','PostController@store')->name('post.store');
 Route::get('/post/show/{id}','PostController@show')->name('post.show');
 Route::delete('/post/{id}','PostController@delete')->name('post.delete');
 
+
+// laravel_test
+// Route::get('/post','PostController@index')->name('post.index');
+// Route::get('/post/create','PostController@create')->name('post.create');
+// Route::post('/post/store','PostController@store')->name('post.store');
+// Route::get('/post/edit/{id}','PostController@edit')->name('post.edit');
+// Route::delete('/post','PostController@delete')->name('post.delete');
+// Route::get('/post/show/{id}','PostController@show')->name('post.show');
+// Route::put('/post/update','PostController@update')->name('post.update');
+
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
