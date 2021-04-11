4f9f42a1282ee55babf5b68fd2cc85b921e1349d
Zac <a19860101@gmail.com>
2021-04-11 11:38:34 +0800

    20210411

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
     function edit(){
         return view('post.edit');
     }
-    function store(){
-        return 'store';
+    function store(Request $request){
+        return $request;
     }
 }
