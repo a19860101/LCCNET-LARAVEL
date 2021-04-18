b8ba547d701bb68c946551ba675d51a0958bd54c
Zac <a19860101@gmail.com>
2021-04-18 11:41:39 +0800

    20210418

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,18 +7,88 @@ use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    # Query Builder
-    //
-    function index(){
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
         $posts = DB::table('posts')->get();
         return view('post.index',compact('posts'));
     }
-    function show($id){
-        // $posts = DB::table('posts')->where('id',$id)->get();
-        // return view('post.show',compact('posts'));
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+
+
+
+        // $posts = DB::table('posts')->where('id',$id)->get();
         // $post = DB::table('posts')->where('id',$id)->first();
         $post = DB::table('posts')->find($id);
         return view('post.show',compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
