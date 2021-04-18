f5119af73f833765f5f154fe257fe8f0c626b1bb
Zac <a19860101@gmail.com>
2021-04-18 14:44:34 +0800

    20210418

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,6 +28,7 @@ class PostController extends Controller
     public function create()
     {
         //
+        return view('post.create');
     }
 
     /**
@@ -39,6 +40,10 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+        $post = new Post;
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->save();
     }
 
     /**
