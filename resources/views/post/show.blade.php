5500c9c49dc25c4acdff39a31a4ecb6324b2f23d
Zac <a19860101@gmail.com>
2021-05-30 11:33:51 +0800

    20210530

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,18 +1,20 @@
 @extends('template.master')
 @section('main')
-
-<h2>{{$post->title}}</h2>
-<div>
-    {{$post->content}}
+<div class="row justify-content-center">
+    <div class="col-lg-8 col-10 border p-5 rounded bg-light">
+        <h1>{{$post->title}}</h1>
+        <div class="my-3">
+            {{$post->content}}
+        </div>
+        <div class="my-3">
+            最後更新時間{{$post->updated_at}}
+        </div>
+        <a href="{{route('post.index')}}" class="btn btn-success">文章列表</a>
+        <a href="{{route('post.edit',['post'=>$post->id])}}" class="btn btn-info">編輯</a>
+        <form action="{{route('post.destroy',['post'=>$post->id])}}" method="post" class="d-inline-block">
+            @csrf
+            @method('delete')
+            <input type="submit" value="刪除" class="btn btn-danger">
+        </form>
 </div>
-<div>
-    最後更新時間{{$post->updated_at}}
-</div>
-<form action="{{route('post.destroy',['post'=>$post->id])}}" method="post">
-    @csrf
-    @method('delete')
-    <input type="submit" value="刪除">
-</form>
-<a href="{{route('post.edit',['post'=>$post->id])}}">編輯</a>
-
 @endsection
