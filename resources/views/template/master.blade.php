2049ab86652a62cc9e10e4105703b7669ac5f8c6
Zac <a19860101@gmail.com>
2021-05-30 10:33:02 +0800

    20210530

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 <body>
-    <nav>
-        <a href="{{route('post.index')}}">文章列表</a>
-        <a href="{{route('post.create')}}">新增文章</a>
-        <a href="{{route('category.create')}}">新增分類</a>
-    </nav>
-    @yield("main")
-    @yield("sidebar")
+    @include("template.nav")
+    <div class="container my-5">
+        @yield("main")
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
