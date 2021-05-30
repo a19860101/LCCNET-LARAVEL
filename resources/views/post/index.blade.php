879a60d77cd5413c78f46eb263be6f64b2dd2074
Zac <a19860101@gmail.com>
2021-05-30 10:44:53 +0800

    20210530

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,23 +1,32 @@
 @extends("template.master")
 @section("main")
-<h1>文章列表</h1>
-
-@foreach($posts as $post)
-<div>
-    <h2>{{$post->title}}</h2>
-    <div>
-        {{$post->category->title}}
+<div class="row justify-content-center">
+    <div class="col-lg-8 col-10">
+        <h1>文章列表</h1>
     </div>
-    <div>
-        {{$post->content}}
-        <a href="{{route('post.show',['post'=>$post->id])}}">繼續閱讀</a>
+    @foreach($posts as $post)
+    <div class="col-lg-8 col-10 border border-secondary rounded p-5 my-3">
+        <div>
+            <h2>{{$post->title}}</h2>
+            <div>
+                分類 <span  class="badge bg-secondary">{{$post->category->title}}</span>
+            </div>
+            <div class="my-3">
+                {{$post->content}}
+            </div>
+            <div class="text-end">
+                <a href="{{route('post.show',['post'=>$post->id])}}" class="btn btn-primary">繼續閱讀</a>
+            </div>
+            <div>
+                最後更新時間 {{$post->updated_at}}
+            </div>
+        </div>
     </div>
-    <div>
-        最後更新時間 {{$post->updated_at}}
-    </div>
-    <hr>
+    @endforeach
 </div>
-@endforeach
+
+
+
 
 @endsection
 
